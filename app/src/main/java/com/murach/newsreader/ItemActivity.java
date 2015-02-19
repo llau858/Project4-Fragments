14dc87bbc3f72d3c7ef3cadd61c0208239d63e04
llau858 <lauschool01@gmail.com>
2015-02-19 10:41:00 -0800

    added my name

--- app/src/main/java/com/murach/newsreader/ItemActivity.java
+++ app/src/main/java/com/murach/newsreader/ItemActivity.java
@@ -1,13 +1,8 @@
 package com.murach.newsreader;
 
-import android.net.Uri;
-import android.os.Bundle;
-import android.view.View;
-import android.view.View.OnClickListener;
-import android.widget.TextView;
 import android.app.Activity;
-import android.content.Intent;
-
+import android.os.Bundle;
+//lisa
 public class ItemActivity extends Activity
 {
 
